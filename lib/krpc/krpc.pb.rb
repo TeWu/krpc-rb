4288e52d3192a21516663015768b77831cc1d1a1
Tomasz Więch <wm.tewu@gmail.com>
2016-11-11 22:14:30 +0100

    Updated protocol buffers schema to version used by kRPC server v0.3.6-71-g0b567c1 (which is pre-release of v0.4.0)

--- lib/krpc/krpc.pb.rb
+++ lib/krpc/krpc.pb.rb
@@ -4,7 +4,30 @@
 require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
+  add_message "krpc.schema.ConnectionRequest" do
+    optional :type, :enum, 1, "krpc.schema.ConnectionRequest.Type"
+    optional :client_name, :string, 2
+    optional :client_identifier, :bytes, 3
+  end
+  add_enum "krpc.schema.ConnectionRequest.Type" do
+    value :RPC, 0
+    value :STREAM, 1
+  end
+  add_message "krpc.schema.ConnectionResponse" do
+    optional :status, :enum, 1, "krpc.schema.ConnectionResponse.Status"
+    optional :message, :string, 2
+    optional :client_identifier, :bytes, 3
+  end
+  add_enum "krpc.schema.ConnectionResponse.Status" do
+    value :OK, 0
+    value :MALFORMED_MESSAGE, 1
+    value :TIMEOUT, 2
+    value :WRONG_TYPE, 3
+  end
   add_message "krpc.schema.Request" do
+    repeated :calls, :message, 1, "krpc.schema.ProcedureCall"
+  end
+  add_message "krpc.schema.ProcedureCall" do
     optional :service, :string, 1
     optional :procedure, :string, 2
     repeated :arguments, :message, 3, "krpc.schema.Argument"
@@ -14,18 +37,19 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :value, :bytes, 2
   end
   add_message "krpc.schema.Response" do
-    optional :time, :double, 1
-    optional :has_error, :bool, 2
-    optional :error, :string, 3
-    optional :has_return_value, :bool, 4
-    optional :return_value, :bytes, 5
+    optional :error, :string, 1
+    repeated :results, :message, 2, "krpc.schema.ProcedureResult"
   end
-  add_message "krpc.schema.StreamMessage" do
-    repeated :responses, :message, 1, "krpc.schema.StreamResponse"
+  add_message "krpc.schema.ProcedureResult" do
+    optional :error, :string, 1
+    optional :value, :bytes, 2
   end
-  add_message "krpc.schema.StreamResponse" do
-    optional :id, :uint32, 1
-    optional :response, :message, 2, "krpc.schema.Response"
+  add_message "krpc.schema.StreamUpdate" do
+    repeated :results, :message, 1, "krpc.schema.StreamResult"
+  end
+  add_message "krpc.schema.StreamResult" do
+    optional :id, :uint64, 1
+    optional :result, :message, 2, "krpc.schema.ProcedureResult"
   end
   add_message "krpc.schema.Services" do
     repeated :services, :message, 1, "krpc.schema.Service"
@@ -40,16 +64,13 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
   add_message "krpc.schema.Procedure" do
     optional :name, :string, 1
     repeated :parameters, :message, 2, "krpc.schema.Parameter"
-    optional :has_return_type, :bool, 3
-    optional :return_type, :string, 4
-    repeated :attributes, :string, 5
-    optional :documentation, :string, 6
+    optional :return_type, :message, 3, "krpc.schema.Type"
+    optional :documentation, :string, 4
   end
   add_message "krpc.schema.Parameter" do
     optional :name, :string, 1
-    optional :type, :string, 2
-    optional :has_default_value, :bool, 3
-    optional :default_value, :bytes, 4
+    optional :type, :message, 2, "krpc.schema.Type"
+    optional :default_value, :bytes, 3
   end
   add_message "krpc.schema.Class" do
     optional :name, :string, 1
@@ -65,7 +86,41 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :value, :int32, 2
     optional :documentation, :string, 3
   end
+  add_message "krpc.schema.Type" do
+    optional :code, :enum, 1, "krpc.schema.Type.TypeCode"
+    optional :service, :string, 2
+    optional :name, :string, 3
+    repeated :types, :message, 4, "krpc.schema.Type"
+  end
+  add_enum "krpc.schema.Type.TypeCode" do
+    value :NONE, 0
+    value :DOUBLE, 1
+    value :FLOAT, 2
+    value :SINT32, 3
+    value :SINT64, 4
+    value :UINT32, 5
+    value :UINT64, 6
+    value :BOOL, 7
+    value :STRING, 8
+    value :BYTES, 9
+    value :CLASS, 100
+    value :ENUMERATION, 101
+    value :PROCEDURE_CALL, 200
+    value :STREAM, 201
+    value :STATUS, 202
+    value :SERVICES, 203
+    value :TUPLE, 300
+    value :LIST, 301
+    value :SET, 302
+    value :DICTIONARY, 303
+  end
+  add_message "krpc.schema.Tuple" do
+    repeated :items, :bytes, 1
+  end
   add_message "krpc.schema.List" do
+    repeated :items, :bytes, 1
+  end
+  add_message "krpc.schema.Set" do
     repeated :items, :bytes, 1
   end
   add_message "krpc.schema.Dictionary" do
@@ -75,11 +130,8 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :key, :bytes, 1
     optional :value, :bytes, 2
   end
-  add_message "krpc.schema.Set" do
-    repeated :items, :bytes, 1
-  end
-  add_message "krpc.schema.Tuple" do
-    repeated :items, :bytes, 1
+  add_message "krpc.schema.Stream" do
+    optional :id, :uint64, 1
   end
   add_message "krpc.schema.Status" do
     optional :version, :string, 1
@@ -106,11 +158,17 @@ end
 
 module KRPC
   module PB
+    ConnectionRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ConnectionRequest").msgclass
+    ConnectionRequest::Type = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ConnectionRequest.Type").enummodule
+    ConnectionResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ConnectionResponse").msgclass
+    ConnectionResponse::Status = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ConnectionResponse.Status").enummodule
     Request = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Request").msgclass
+    ProcedureCall = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ProcedureCall").msgclass
     Argument = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Argument").msgclass
     Response = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Response").msgclass
-    StreamMessage = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.StreamMessage").msgclass
-    StreamResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.StreamResponse").msgclass
+    ProcedureResult = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.ProcedureResult").msgclass
+    StreamUpdate = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.StreamUpdate").msgclass
+    StreamResult = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.StreamResult").msgclass
     Services = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Services").msgclass
     Service = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Service").msgclass
     Procedure = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Procedure").msgclass
@@ -118,11 +176,14 @@ module KRPC
     Class = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Class").msgclass
     Enumeration = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Enumeration").msgclass
     EnumerationValue = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.EnumerationValue").msgclass
+    Type = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Type").msgclass
+    Type::TypeCode = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Type.TypeCode").enummodule
+    Tuple = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Tuple").msgclass
     List = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.List").msgclass
+    Set = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Set").msgclass
     Dictionary = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Dictionary").msgclass
     DictionaryEntry = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.DictionaryEntry").msgclass
-    Set = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Set").msgclass
-    Tuple = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Tuple").msgclass
+    Stream = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Stream").msgclass
     Status = Google::Protobuf::DescriptorPool.generated_pool.lookup("krpc.schema.Status").msgclass
   end
 end
