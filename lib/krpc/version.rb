bbcbf55cf524948be6206b8f4a0b53f7d6bd5679
Tomasz Więch <wm.tewu@gmail.com>
2016-11-18 19:28:51 +0100

    Created KRPC::Version module to support more granular version checks

--- lib/krpc/version.rb
+++ lib/krpc/version.rb
@@ -1,3 +1,10 @@
 module KRPC
-  VERSION = "0.3.1"
+  module Version
+    MAJOR = 0
+    MINOR = 3
+    PATCH = 1
+    LABEL = nil
+  end
+
+  VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL].compact * '.').freeze
 end
