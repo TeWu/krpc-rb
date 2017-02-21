7de1375360df295cd2e3b27403312b448c5eba3a
Tomasz Więch <wm.tewu@gmail.com>
2017-02-21 20:12:33 +0100

    Bump to 0.4.0.beta2

--- lib/krpc/version.rb
+++ lib/krpc/version.rb
@@ -3,7 +3,7 @@ module KRPC
     MAJOR = 0
     MINOR = 4
     PATCH = 0
-    LABEL = 'beta1'
+    LABEL = 'beta2'
   end
 
   VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL].compact * '.').freeze
