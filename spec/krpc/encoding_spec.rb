78c8c1c5acea3f04f6cceaf1e0e33ecf1c77aa35
Tomasz Więch <wm.tewu@gmail.com>
2017-07-03 16:04:36 +0200

    Refactor encoding_spec

--- spec/krpc/encoding_spec.rb
+++ spec/krpc/encoding_spec.rb
@@ -8,115 +8,115 @@ RSpec.describe "protocol buffer encoding" do
 
 
   it "encodes float" do
-    t = get_type :FLOAT
-    check_close 0, "00000000", t
-    check_close -1.0, "000080bf", t
-    check_close 3.14159265359, "db0f4940", t
+    c = create_checker :check_close, :FLOAT
+    c.( 0, "00000000" )
+    c.( -1.0, "000080bf" )
+    c.( 3.14159265359, "db0f4940" )
   end
 
   it "encodes double" do
-    t = get_type :DOUBLE
-    check_close 0.0, "0000000000000000", t
-    check_close -1.0, "000000000000f0bf", t
-    check_close 3.14159265359, "ea2e4454fb210940", t
+    c = create_checker :check_close, :DOUBLE
+    c.( 0.0, "0000000000000000" )
+    c.( -1.0, "000000000000f0bf" )
+    c.( 3.14159265359, "ea2e4454fb210940" )
   end
 
   it "encodes sint32" do
-    t = get_type :SINT32
-    check_equal 0, "00", t
-    check_equal 1, "02", t
-    check_equal 42, "54", t
-    check_equal 1108, "a811", t
-    check_equal -1, "01", t
-    check_equal -33, "41", t
-    check_equal -1621, "a919", t
+    c = create_checker :check_equal, :SINT32
+    c.( 0, "00" )
+    c.( 1, "02" )
+    c.( 42, "54" )
+    c.( 1108, "a811" )
+    c.( -1, "01" )
+    c.( -33, "41" )
+    c.( -1621, "a919" )
   end
 
   it "encodes sint64" do
-    t = get_type :SINT64
-    check_equal 0, "00", t
-    check_equal 1, "02", t
-    check_equal 42, "54", t
-    check_equal 1108, "a811", t
-    check_equal -1, "01", t
-    check_equal -33, "41", t
-    check_equal -1621, "a919", t
-    check_equal 1234567890000, "a091d89fee47", t
+    c = create_checker :check_equal, :SINT64
+    c.( 0, "00" )
+    c.( 1, "02" )
+    c.( 42, "54" )
+    c.( 1108, "a811" )
+    c.( -1, "01" )
+    c.( -33, "41" )
+    c.( -1621, "a919" )
+    c.( 1234567890000, "a091d89fee47" )
   end
 
   it "encodes uint32" do
-    t = get_type :UINT32
-    check_equal 0, "00", t
-    check_equal 1, "01", t
-    check_equal 42, "2a", t
-    check_equal 300, "ac02", t
+    c = create_checker :check_equal, :UINT32
+    c.( 0, "00" )
+    c.( 1, "01" )
+    c.( 42, "2a" )
+    c.( 300, "ac02" )
   end
 
   it "encodes uint64" do
-    t = get_type :UINT64
-    check_equal 0, "00", t
-    check_equal 1, "01", t
-    check_equal 42, "2a", t
-    check_equal 300, "ac02", t
-    check_equal 1234567890000, "d088ec8ff723", t
+    c = create_checker :check_equal, :UINT64
+    c.( 0, "00" )
+    c.( 1, "01" )
+    c.( 42, "2a" )
+    c.( 300, "ac02" )
+    c.( 1234567890000, "d088ec8ff723" )
   end
 
   it "encodes bool" do
-    t = get_type :BOOL
-    check_equal true, "01", t
-    check_equal false, "00", t
+    c = create_checker :check_equal, :BOOL
+    c.( true, "01" )
+    c.( false, "00" )
   end
 
   it "encodes string" do
-    t = get_type :STRING
-    check_equal "", "00", t
-    check_equal "testing", "0774657374696e67", t
-    check_equal "One small step for Kerbal-kind!", "1f4f6e6520736d616c6c207374657020666f72204b657262616c2d6b696e6421", t
-    check_equal "\xe2\x84\xa2", "03e284a2", t
-    check_equal "Mystery Goo\xe2\x84\xa2 Containment Unit", "1f4d79737465727920476f6fe284a220436f6e7461696e6d656e7420556e6974", t
+    c = create_checker :check_equal, :STRING
+    c.( "", "00" )
+    c.( "testing", "0774657374696e67" )
+    c.( "One small step for Kerbal-kind!", "1f4f6e6520736d616c6c207374657020666f72204b657262616c2d6b696e6421" )
+    c.( "\xe2\x84\xa2", "03e284a2" )
+    c.( "Mystery Goo\xe2\x84\xa2 Containment Unit", "1f4d79737465727920476f6fe284a220436f6e7461696e6d656e7420556e6974" )
   end
 
   it "encodes bytes" do
-    t = get_type :BYTES
-    check_equal [], "00", t
-    check_equal [0xBA,0xDA,0x55], "03bada55", t
-    check_equal [0xDE,0xAD,0xBE,0xEF], "04deadbeef", t
+    c = create_checker :check_equal, :BYTES
+    c.( [], "00" )
+    c.( [0xBA,0xDA,0x55], "03bada55" )
+    c.( [0xDE,0xAD,0xBE,0xEF], "04deadbeef" )
   end
 
   it "encodes list" do
-    t = get_type :LIST, type_codes: [:SINT32]
-    check_equal [], "", t
-    check_equal [1], "0a0102", t
-    check_equal [1,2,3,4], "0a01020a01040a01060a0108", t
+    c = create_checker :check_equal, get_type(:LIST, type_codes: [:SINT32])
+    c.( [], "" )
+    c.( [1], "0a0102" )
+    c.( [1,2,3,4], "0a01020a01040a01060a0108" )
   end
 
   it "encodes dictionary" do
-    t = get_type :DICTIONARY, type_codes: [:STRING, :SINT32]
-    check_equal({}, "", t)
-    check_equal({"" => 0}, "0a060a0100120100", t)
-    check_equal({"foo" => 42, "bar" => 365, "baz" => 3}, "0a090a0403666f6f1201540a0a0a04036261721202da050a090a040362617a120106", t)
+    c = create_checker :check_equal, get_type(:DICTIONARY, type_codes: [:STRING, :SINT32])
+    c.( {}, "" )
+    c.( {"" => 0}, "0a060a0100120100" )
+    c.( {"foo" => 42, "bar" => 365, "baz" => 3}, "0a090a0403666f6f1201540a0a0a04036261721202da050a090a040362617a120106" )
   end
 
   it "encodes set" do
-    t = get_type :SET, type_codes: [:UINT32]
-    check_equal [].to_set, "", t
-    check_equal [1].to_set, "0a0101", t
-    check_equal [1,2,3,4].to_set, "0a01010a01020a01030a0104", t
+    c = create_checker :check_equal, get_type(:SET, type_codes: [:UINT32])
+    c.( [].to_set, "" )
+    c.( [1].to_set, "0a0101" )
+    c.( [1,2,3,4].to_set, "0a01010a01020a01030a0104" )
   end
 
   it "encodes tuple" do
     t1 = get_type :TUPLE, type_codes: [:UINT32]
-    check_equal [1], "0a0101", t1
+    check_equal t1, [1], "0a0101"
 
     t2 = get_type :TUPLE, type_codes: [:UINT32, :STRING, :BOOL]
-    check_equal [1,"jeb",false], "0a01010a04036a65620a0100", t2
+    check_equal t2, [1,"jeb",false], "0a01010a04036a65620a0100"
   end
 
   it "encodes message" do
     t = get_type :PROCEDURE_CALL
     msg = PB::ProcedureCall.new(service: "ServiceName", procedure: "ProcedureName")
     data = "0a0b536572766963654e616d65120d50726f6365647572654e616d65"
-    check_equal msg, data, t
+    check_equal t, msg, data
 
     t2 = get_type :STATUS
     msg2 = PB::Status.new(
@@ -133,7 +133,7 @@ RSpec.describe "protocol buffer encoding" do
       stream_rpcs: 19
     )
     data2 = "0a036f6e65100225000090402d6666d64030083d9a9911414001480b5801800113"
-    check_equal msg2, data2, t2
+    check_equal t2, msg2, data2
   end
 
   it "encodes message with size" do
@@ -158,12 +158,17 @@ RSpec.describe "protocol buffer encoding" do
     s.scan(/../).map { |x| x.hex.chr }.join
   end
 
-  def check_close(value, data, type, delta = 0.0001)
+  def create_checker(method_name, type)
+    type = get_type(type) if type.is_a? Symbol
+    method(method_name).to_proc.curry.(type)
+  end
+
+  def check_close(type, value, data, delta = 0.0001)
     expect(hexlify(Encoder.encode(value, type))).to eq data
     expect(Decoder.decode(unhexlify(data), type, :clientless)).to be_within(delta).of(value)
   end
 
-  def check_equal(value, data, type)
+  def check_equal(type, value, data)
     expect(hexlify(Encoder.encode(value, type))).to eq data
     expect(Decoder.decode(unhexlify(data), type, :clientless)).to eq value
   end
