957d8f0c99ba7406a2837d39a4a2ea6a440969d8
Tomasz Więch <tewu.dev@gmail.com>
2020-03-27 12:36:28 +0100

    Mark as unstable version

--- lib/krpc/version.rb
+++ lib/krpc/version.rb
@@ -5,7 +5,8 @@ module KRPC
     MINOR = 4
     PATCH = 0
     LABEL = nil
+    IS_STABLE = false
   end
 
-  VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL].compact * '.').freeze
+  VERSION = ([Version::MAJOR, Version::MINOR, Version::PATCH, Version::LABEL, Version::IS_STABLE ? nil : "next"].compact * '.').freeze
 end
